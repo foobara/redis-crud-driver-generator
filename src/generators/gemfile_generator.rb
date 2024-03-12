55ef096f1d11b6c5dc13ee40d241ea690c89883e
Miles Georgi <azimux@gmail.com>
2024-03-11 17:12:56 -0700

    Don't update gemfile if gem is already there

--- src/generators/gemfile_generator.rb
+++ src/generators/gemfile_generator.rb
@@ -5,6 +5,10 @@ module Foobara
         # Kind of tricky... for the first time we will be loading an existing file in the working directory
         # and modifying it.
         class GemfileGenerator < RedisCrudDriverGenerator
+          def applicable?
+            gemfile_contents !~ /^\s*gem\s*["']foobara-redis-crud-driver\b/
+          end
+
           def template_path
             "Gemfile"
           end
@@ -14,9 +18,7 @@ module Foobara
           end
 
           def generate(_elements_to_generate)
-            contents = File.read(template_path)
-
-            match = contents.match(/^gem /)
+            match = gemfile_contents.match(/^gem /)
 
             if match
               new_entry = 'gem "foobara-redis-crud-driver", github: "foobara/redis-crud-driver"'
@@ -27,6 +29,10 @@ module Foobara
               raise "Not sure how to inject redis_crud_driver into the Gemfile"
               # :nocov:
             end
+          end
+
+          def gemfile_contents
+            File.read(template_path)
           end
         end
       end
